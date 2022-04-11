3ff16515329a0e5759491746a5b454944a168498
Rodolfo Fonseca 01 <rodolfofonseca01@gmail.com>
2022-04-11 07:34:13 -0300

    preparação do ambiente para o trabalho

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/bancoDeDados/vendor/autoload.php';
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
-$factory = (new Factory())->withServiceAccount(__DIR__.'/firebaseconfig.json')->withDatabaseUri('https://portaldenoticias-52a35-default-rtdb.firebaseio.com/');
+$factory = (new Factory())->withServiceAccount(__DIR__.'/bancoDeDados/firebaseconfig.json')->withDatabaseUri('https://portaldenoticias-52a35-default-rtdb.firebaseio.com/');
 $database = $factory->createDatabase();
 /**
  * Busco todos os contatos que possue na databse
